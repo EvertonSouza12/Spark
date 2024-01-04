3503139320f8c7841868f23454c944888fb869af
Everton Souza <souza1287@live.com>
2024-01-04 20:28:59 -0300

    var e val types

---
+++ basics-scala/src/main/scala/part1basics/vartypes.scala
@@ -0,0 +1,26 @@
+package part1basics
+
+object vartypes extends App {
+
+  //val é imutavel / tipagem dinamica
+  val x: Int = 42
+  println(x)
+
+  val aString: String = "Hello"
+  val anotherString = "Goodbye"
+  println(aString)
+  println(anotherString)
+
+  val aboolean: Boolean = true
+  val aChar: Char = "x"
+  val aInt: Int = 2
+  val aShort: Short = 4613
+  val aLong: Long = 2312312343242L
+  val aFloat: Float = 3.0f
+  val aDouble: Double = 2.4
+
+
+  //variaveis / são mutaveis
+  var aVariable: Int = 4
+  aVariable = 2 //Side effects
+}
